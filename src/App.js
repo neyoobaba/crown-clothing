af2360724f7cd829323f27dbdaa3066a13797d4d
Francis Oyekunle <foyekunle@flexpay.co.za>
2021-07-27 18:02:43 +0200

    adding header commponent and links to come, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { SwitchCase, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
+
 
 function App() {
   return  (
-  <div >
-    <switch>
-    <Route exact path='/' component={HomePage} />
-    <Route path='/shop' component={ShopPage} />
-    </switch>
-    
-  </div>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+      
+    </div>
   );
 }
 
