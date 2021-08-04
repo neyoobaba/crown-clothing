8205706c4982a2a77a164e136cb70688637a2119
Francis Oyekunle <foyekunle@flexpay.co.za>
2021-08-04 20:18:14 +0200

    implemented firebase utils, included ability to store authenticated user into firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
     appId: "1:33337373444:web:fed21734c6ed4c17499651"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createDate = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createDate,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 
